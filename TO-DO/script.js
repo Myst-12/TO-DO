307cd9e9ee7c05be4523ce522089aced4eb81a06
Myst-12 <kushal11.mittal@gmail.com>
2025-12-30 05:09:51 +0530

    amost done, styling left

--- TO-DO/script.js
+++ TO-DO/script.js
@@ -4,10 +4,33 @@ add.addEventListener("click", handle)
 
 function handle(){
     input = document.getElementById("input")
-    list.innerHTML += "<div class='list-items'><input type='checkbox' class='list-checkbox'> <label class='list-label'>" + input.value + "</label></div>"
-    input.value = ""
+    if (input.value.trim()==""){
+        add.style.backgroundColor="red"
+    }
+    else{
 
-    list_checkbox = document.getElementsByClassName("list-checkbox")
+        add.style.backgroundColor="rgb(130, 229, 130)"
+        list.innerHTML += "<div class='list-items'><input type='checkbox' class='list-checkbox'> <label class='list-label'>" + input.value + "</label></div>"
+        input.value = ""
+
+        list_checkbox = document.getElementsByClassName("list-checkbox")
+        list_items = document.getElementsByClassName("list-items")
+        for (let i=0; i<list_checkbox.length; i++){
+            list_checkbox[i].addEventListener("click", wipe)
+        }
+        
+        function wipe(){
+        let item = this.parentElement
+        item.classList.add("done")
+        setTimeout(() => {
+            item.innerHTML = ""
+        }, 1000)
+        }
+    }
+    
+
     
 }
+
+
 
